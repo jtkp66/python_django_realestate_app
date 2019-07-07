cd4396a8c78db373989aecdaaa8099cbc557603b
Jamie Pham <jtkp66@gmail.com>
2019-07-06 17:44:28 -0700

    Connected django with postgresql

--- btre/settings.py
+++ btre/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "btredb",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5001",
     }
 }
 
